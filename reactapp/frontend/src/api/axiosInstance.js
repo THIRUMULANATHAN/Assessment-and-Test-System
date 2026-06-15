0c7f8595b8aaf97b0eadaac7596deb11d71ce155
Thiru <thiru2005v@gmail.com>
2026-06-15 21:27:33 +0530

    Connect frontend with deployed backend

--- reactapp/frontend/src/api/axiosInstance.js
+++ reactapp/frontend/src/api/axiosInstance.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://assessment-and-test-system.onrender.com",
+  baseURL: "https://assessment-and-test-system.onrender.com/api",
 });
 
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
