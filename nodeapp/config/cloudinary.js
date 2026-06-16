15ba645e32b6b40af41cc56ded1b0047a38f23a7
Thiru <thiru2005v@gmail.com>
2026-06-16 10:39:51 +0530

    Secure Cloudinary config

--- nodeapp/config/cloudinary.js
+++ nodeapp/config/cloudinary.js
@@ -1,9 +1,15 @@
-const cloudinary = require('cloudinary').v2;
+// nodeapp/config/cloudinary.js
+
+const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'dv0jouxui',
-  api_key: process.env.CLOUDINARY_API_KEY || '892264379247193',
-  api_secret: process.env.CLOUDINARY_API_SECRET || 'D6fMK_RBUKGuTfM1bNeJb410Fec',
+
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+
+  api_key: process.env.CLOUDINARY_API_KEY,
+
+  api_secret: process.env.CLOUDINARY_API_SECRET
+
 });
 
 module.exports = cloudinary;
